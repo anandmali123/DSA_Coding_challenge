1fe7d1883fc5ef4ec95531d189f6266c5962a36f
anandmali123 <malianand247@gmail.com>
2024-03-10 23:46:46 +0530

    Day 56

---
+++ Day 56/remove_duplicates_from_Sorted_linkedlist.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null)
+            return null;
+
+        ListNode temp = head;
+
+        while (temp != null) {
+            while (temp.next != null && temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
+}
